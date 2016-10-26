3aed69089a44d8a4f4b452f52e374b56b66b15d9
esteban1234 <tva_libra@hotmail.com>
2016-10-26 09:59:51 -0500

    icono de basura

--- core/models/class.Opciones.php
+++ core/models/class.Opciones.php
@@ -1,31 +1,32 @@
 <?php
-    class Opciones
+class Opciones
+{
+    public function __construct()
     {
-        public function __construct()
-        {
-            $this->db = new Conexion();
-        }
+        $this->db = new Conexion();
+    }
 
-        public function getTAREA()
-        {
-            $sql = $this->db->query("SELECT * FROM agregar;");
-            $salida = '';
-            while ($row = $this->db->runs($sql)) {
+    public function getTAREA()
+    {
+        $sql    = $this->db->query("SELECT * FROM agregar;");
+        $salida = '';
+        while ($row = $this->db->runs($sql)) {
 
-                $salida .= '<tr>
+            $salida .= '<tr>
                   <td>' . $row['asunto'] . '</td>
                   <td>' . $row['hora_realizarla'] . '</td>
                   <td>' . $row['fecha_alta'] . '</td>
                   <td>' . $row['fecha_realizar'] . '</td>
                   <td>' . $row['agregar_tarea'] . '</td>
-                  <td><a data-accion="deleteTAREA" href="'.$row['id'].'"><button type="button" class="btn btn-danger" id="" data-toggle="modal" data-target="#dialog-borrar">Eliminar</button></a>
+                  <td><a data-accion="deleteTAREA" href="' . $row['id'] . '"><button type="button" class="btn btn-danger" id="" data-toggle="modal" data-target="#dialog-borrar"><i class="icon-trash"></i>&nbsp;Eliminar</button></a>
                   </td></tr>';
 
-            }
-                //}<td><a data-accion="deleteTAREA"  href="'.$row['id'].'"><button type="button" class="btn btn-danger" data-toggle="modal" data-target="#dialog-borrar"><i class="icon-trash"></i>&nbsp;Eliminar</button></a>'
-
-                return $salida;
         }
-    }
+        //}<td><a data-accion="deleteTAREA"  href="'.$row['id'].'"><button type="button" class="btn btn-danger" data-toggle="modal" data-target="#dialog-borrar"><i class="icon-trash"></i>&nbsp;Eliminar</button></a>'
 
-?>
+        // <a class="btn btn-danger" href="#">
+        // <i class="icon-trash"></i>&nbsp;Eliminar</a>
+
+        return $salida;
+    }
+}
