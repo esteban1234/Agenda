c6846291ad5430ae6d1424b52458af50865a9807
champiiy <albert1047@hotmail.com>
2016-10-27 17:15:38 -0500

    Mensaje de eliminado ocultarse

--- views/app/js/goRESULTADO.js
+++ views/app/js/goRESULTADO.js
@@ -22,13 +22,22 @@ $('#eliminar').on('click',function(){
         url:'ajax.php?mode=opciones',
         data:'accion='+accion+'&id='+idUSER,
         success: function(response){
+            $('#_AJAX_REG_').fadeIn(500);
             $('#refresca').empty();
             $('#refresca').append(response.contenido);
             $('#_AJAX_REG_').append(response.mensaje);
+            setTimeout(deleteMSJ,1000);
             $('#dialog-borrar').modal('toggle');
+
+            //$('#_AJAX_REG_').empty();
         },
         error: function(){
             alert('Error del sistema, Vuelva a intentar');
         }
     });
 });
+
+function deleteMSJ() {
+    $('#_AJAX_REG_').fadeOut(1500);
+    $('#_AJAX_REG_').empty();
+}
